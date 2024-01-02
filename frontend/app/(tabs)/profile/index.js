22a78807457da9f1fb346a3fae19eda296dab967
Fauzan <ojanaja03@gmail.com>
2024-01-03 01:00:39 +0700

    fix: folder structure

--- frontend/app/(tabs)/profile/index.js
+++ frontend/app/(tabs)/profile/index.js
@@ -17,56 +17,52 @@ const index = () => {
       console.log("error", error)
     }
   }
+
   useEffect(() => {
     fetchTaskData();
   }, []);
+
   console.log("comp", completedTasks);
   console.log("pending", pendingTasks);
 
   return (
-    <View style={{padding:10,flex:1,backgroundColor:"white"}}>
+    <View style={{ padding: 10, flex: 1, backgroundColor: "white" }}>
       <View style={{ flexDirection: "row", alignItems: "center", gap: 10 }}>
-        // <Image
-        //   style={{ width: 60, height: 60, borderRadius: 30 }}
-        //   source={{
-        //     uri: "https://lh3.googleusercontent.com/ogw/ANLem4Zmk7fohWyH7kB6YArqFy0WMfXnFtuX3PX3LSBf=s64-c-mo",
-        //   }}
-        // />
         <View>
           <Text style={{ fontSize: 16, fontWeight: "600" }}>This is your Profile</Text>
           <Text style={{ fontSize: 15, color: "gray", marginTop: 4 }}>Detail</Text>
         </View>
       </View>
 
-      <View style={{marginVertical:12}}>
+      <View style={{ marginVertical: 12 }}>
         <Text>Tasks Overview</Text>
-        <View style={{flexDirection:"row",alignItems:"center",gap:6,marginVertical:8}}>
+        <View style={{ flexDirection: "row", alignItems: "center", gap: 6, marginVertical: 8 }}>
           <View style={{
             backgroundColor: "#89cff0",
             padding: 10,
-            borderRadius:8,
+            borderRadius: 8,
             flex: 1,
             justifyContent: "center",
             alignItems: "center"
           }}>
-            <Text style={{textAlign:"center",fontSize:16,fontWeight:"bold"}}>{completedTasks}</Text>
-            <Text style={{marginTop:4}}>Completed Tasks</Text>
+            <Text style={{ textAlign: "center", fontSize: 16, fontWeight: "bold" }}>{completedTasks}</Text>
+            <Text style={{ marginTop: 4 }}>Completed Tasks</Text>
           </View>
 
           <View style={{
             backgroundColor: "#89cff0",
             padding: 10,
-            borderRadius:8,
+            borderRadius: 8,
             flex: 1,
             justifyContent: "center",
             alignItems: "center"
           }}>
-            <Text style={{textAlign:"center",fontSize:16,fontWeight:"bold"}}>{pendingTasks}</Text>
-            <Text style={{marginTop:4}}>Pending Tasks</Text>
+            <Text style={{ textAlign: "center", fontSize: 16, fontWeight: "bold" }}>{pendingTasks}</Text>
+            <Text style={{ marginTop: 4 }}>Pending Tasks</Text>
           </View>
         </View>
       </View>
-      
+
       <LineChart
         data={{
           labels: ["Pending Tasks", "Completed Tasks"],
@@ -78,8 +74,6 @@ const index = () => {
         }}
         width={Dimensions.get("window").width - 20} // from react-native
         height={220}
-        // yAxisLabel="$"
-        // yAxisSuffix="k"
         yAxisInterval={2} // optional, defaults to 1
         chartConfig={{
           backgroundColor: "#e26a00",
@@ -103,8 +97,8 @@ const index = () => {
         }}
       />
 
-      <View style={{backgroundColor:"#89CFF0", padding:10,borderRadius:6,marginTop:15}}>
-        <Text style={{textAlign:"center", color:"white"}}>Tasks for the next 7 days</Text>  
+      <View style={{ backgroundColor: "#89CFF0", padding: 10, borderRadius: 6, marginTop: 15 }}>
+        <Text style={{ textAlign: "center", color: "white" }}>Tasks for the next 7 days</Text>
       </View>
 
       <View
@@ -121,7 +115,6 @@ const index = () => {
           }}
         />
       </View>
-      
     </View>
   )
 }
